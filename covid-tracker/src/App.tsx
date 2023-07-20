f1f8d1850e2d1eaa4f863dd4ff2fdade124d6c7c
TrungAnh <81064478+AnhHT3010@users.noreply.github.com>
2023-07-20 11:11:14 +0700

    fix: app route

--- covid-tracker/src/App.tsx
+++ covid-tracker/src/App.tsx
@@ -1,11 +1,27 @@
 import React from 'react';
 import './App.css';
-import { Counter } from './features/counter/Counter';
+import Login from './pages/Login/Login';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import ForgotPassword from './pages/ForgotPassword/ForgotPassword';
+import Home from './pages/UserPage/Home/Home';
 
 function App() {
-  return <div className="App">{
-    <Counter />
-  }</div>;
+  return (
+    <div className="App">
+      {
+        <BrowserRouter>
+          <Routes>
+            <Route path="/portal/login-organ" element={<Login />} />
+            <Route
+              path="/portal/password-forgot"
+              element={<ForgotPassword />}
+            />
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </BrowserRouter>
+      }
+    </div>
+  );
 }
 
 export default App;
