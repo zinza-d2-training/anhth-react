86d56a7fe57de52289a3fbe013675501860ca529
TrungAnh <81064478+AnhHT3010@users.noreply.github.com>
2023-07-21 11:41:19 +0700

    Implement login (#13)
    * implement-login
    
    * fix: store
    
    * fix: app route
    
    * fix:remove ignore yarnlock

--- covid-tracker/src/store.ts
+++ covid-tracker/src/store.ts
@@ -1,10 +1,26 @@
-import { configureStore } from '@reduxjs/toolkit';
-import counterSlice from './features/counter/counterSlice';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import userSlice from './store/userSlice';
+
+const rootReducer = combineReducers({
+  user: userSlice
+});
+
+const persistConfig = {
+  key: 'root',
+  storage: storage,
+  blacklist: []
+};
+
+const persistReducers = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-  reducer: {
-    counter: counterSlice
-  }
+  reducer: persistReducers,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false
+    })
 });
 
 export type RootState = ReturnType<typeof store.getState>;
